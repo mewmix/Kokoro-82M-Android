f1f5d9b275d1ceb57371c9e1362eeeb2e8b5099f
Alex Klein <alex@facings.io>
2025-08-03 17:10:26 -0700

    fix import

--- app/src/main/java/com/example/kokoro82m/screens/BookScreen.kt
+++ app/src/main/java/com/example/kokoro82m/screens/BookScreen.kt
@@ -7,6 +7,7 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
